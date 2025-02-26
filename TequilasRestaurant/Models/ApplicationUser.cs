4ce722e99775a2ccecc71aacc58cc84bcc3da175
Amberlie Hicken <amberlie.hicken@gmail.com>
2025-02-26 02:21:15 -0700

    Add project files.

---
+++ TequilasRestaurant/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TequliasRestaurant.Models;
+
+namespace TequilasRestaurant.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+         public ICollection<Order>? Orders { get; set;}
+    }
+}
